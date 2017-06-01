e8e933007af16fe430cca87d8098170b180baa94
sophiedrodge <sjdro1@student.monash.edu>
2017-06-02 00:03:47 +1000

    finishing

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,23 +4,14 @@ $(document).ready(function() {
 // Add jQuery here
 
 
+$('.landscape').mouseenter(function() {
+  $('.landscape__img').addClass('showme');
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$('.landscape').mouseleave(function() {
+  $('.landscape__img').removeClass('showme');
+});
 
 
 
